c1ce3d08fa7414ad55752b623349edb17715b08d
woaiot5 <tmtwabp@gmail.com>
2025-04-02 12:59:02 +0200

    optimisation and scoreboard

--- hangedman_countries_game/constants.py
+++ hangedman_countries_game/constants.py
@@ -1,4 +1,20 @@
-stages = [r'''
+from os import path
+import json
+
+with open(f'{path.dirname( __file__ )}/countries.json') as country_json:
+    COUNTRIES_INFO = json.load(country_json)
+
+def letters_to_chose():
+    keyboard = "qwertyuiopasdfghjklzxcvbnm".upper()
+    all_letters = []
+    for letter in keyboard:
+        all_letters.append(letter)
+    all_letters.sort()
+    return all_letters
+
+LETTERS = letters_to_chose()
+STARS = "***********************"
+STAGES = [r'''
   +---+
   |   |
   O   |
@@ -62,3 +78,4 @@ stages = [r'''
       |
 =========
 ''']
+NR_STAGES = len(STAGES) - 1
--- hangedman_countries_game/main.py
+++ hangedman_countries_game/main.py
@@ -1,114 +1,142 @@
+from os import system, name
 import random
-from stages import stages
-import json
-
-STARS = "***********************"
-
-def letters_to_chose():
-    keyboard = "qwertyuiopasdfghjklzxcvbnm".upper()
-    all_letters = []
-    for letter in keyboard:
-        all_letters.append(letter)
-    all_letters.sort()
-    return all_letters
+from constants import STARS, LETTERS, STAGES, NR_STAGES, COUNTRIES_INFO
 
 
 def letters_in_word(input_word):
+    """Returns a dictionary where the key is a sequence number of a letter or a symbol in an input string,
+    and value is a nested dictionary of 'symbol' and 'display'.
+    Alphabet letters are displayed as '_', other symbols stay the same."""
+
     word_dict = {}
     for letter in input_word:
-        guess = "_"
-        if letter not in letters_to_chose(): guess = letter
-        word_dict[len(word_dict)] = {"letter": letter, "guess": guess}
+        answer = "_"
+        if letter not in LETTERS:
+            answer = letter
+        word_dict[len(word_dict)] = {"symbol": letter, "display": answer}
     return word_dict
 
 
 def def_display_line (input_word):
     line = ""
     for item in input_word:
-        line += input_word[item]["guess"]
+        line += input_word[item]["display"]
     return line
+
+
+def print_header(lives_left, country):
+    system('cls' if name == 'nt' else 'clear')
+    print(f'{STARS} Lets play Hanged Man! {STARS}')
+    print(f"{STARS} {lives_left}/{NR_STAGES} LIVES LEFT {STARS} ")
+    print(f"{STARS} Guess a country {STARS}")
+    print(f"{len(country["name"].upper())} symbols:\n{country["display"]}")
+    print(STAGES[lives])
 
 
 def update_guess (input_word, guessed_letter):
     for item in input_word:
-        if input_word[item]["letter"] == guessed_letter:
-            input_word[item]["guess"] = guessed_letter
+        if input_word[item]["symbol"] == guessed_letter:
+            input_word[item]["display"] = guessed_letter
     return input_word
 
 
 def give_a_hint(hints_list, country):
     hints_dict = {
-        0: f"{STARS} HINT: this country is located in {country["region"]} {STARS}",
-        1: f"{STARS} HINT: official currency of this country is {country["currency"]["code"]} {country["currency"]["symbol"]} {STARS}",
-        2: f"{STARS} HINT: capital of this country is {country["capital"]} {STARS}",
-        3: f"{STARS} HINT: official language of this country is {country["language"]["name"]} {STARS}"
+        0: f"\n{STARS} HINT: this country is located in {country["region"]} {STARS}",
+        1: f"\n{STARS} HINT: official currency of this country is {country["currency"]["code"]} {country["currency"]["symbol"]} {STARS}",
+        2: f"\n{STARS} HINT: capital of this country is {country["capital"]} {STARS}",
+        3: f"\n{STARS} HINT: official language of this country is {country["language"]["name"]} {STARS}"
     }
     hint_nr = random.choice(hints_list)
     hints_list.remove(hint_nr)
     print(hints_dict[hint_nr])
+    return hints_dict[hint_nr]
 
 
-letters = letters_to_chose()
+def lose_or_win(lives_left, is_game_over):
+    if lives_left == 0:
+        is_game_over = True
+        system('cls' if name == 'nt' else 'clear')
+        print(f"{STARS} YOU LOSE {STARS}\n{STARS} IT WAS <{selected_country["name"].upper().upper()}>! {STARS} ")
 
-with open('countries.json') as country_json:
-  country_dict = json.load(country_json)
+    if "_" not in selected_country["display"]:
+        is_game_over = True
+        system('cls' if name == 'nt' else 'clear')
+        print(f"{STARS} YOU WIN {STARS}")
+
+    return is_game_over
+
+
+def give_country_info(country):
+    print(
+        f"\n{country["name"]} is located in {country["region"]}, it's capital is {country["capital"]}, "
+        f"official language is {country["language"]["name"]} and currency is {country["currency"]["name"]}!\n"
+    )
+
+
+def print_footer():
+    system('cls' if name == 'nt' else 'clear')
+    print(f"{STARS} Bye-bye! {STARS}")
+
 
 game_on = True
+wins = 0
+loses = 0
+streak = 0
 
 while game_on:
     game_over = False
-    lives = 7
+    lives = len(STAGES) - 1
     hints = [0, 1, 2, 3]
     all_guesses = []
-
-    selected_country = random.choice(country_dict)
-    country_name = selected_country["name"].upper()
-    country_letters = letters_in_word(country_name)
-    display = def_display_line(country_letters)
-
-    print(f'{STARS} Lets play Hanged Man! {STARS}\n{STARS} Guess a country {STARS}\n{len(country_name)} symbols:\n{display}')
+    my_hints = []
+    result = ""
+    selected_country = random.choice(COUNTRIES_INFO)
+    selected_country["letters"] = letters_in_word(selected_country["name"].upper())
+    selected_country["display"] = def_display_line(selected_country["letters"])
 
     while not game_over:
-        print(f"{STARS} {lives}/{len(stages)-1} LIVES LEFT {STARS} ")
-        if len(all_guesses) >0:
+
+        print_header(lives,selected_country)
+
+        if len(all_guesses) > 0:
             print(f"Your guesses: {', '.join(str(x) for x in all_guesses)}")
-        guess = input("Guess a letter or country name: ").upper()
+
+        if len(my_hints) > 0:
+            print(*my_hints, sep='\n')
+
+        guess = input(f"\nGuess a letter or country name: ").upper()
 
         while guess in all_guesses:
             guess = input("This letter has been already guessed, enter another letter: ").upper()
         all_guesses.append(guess)
 
-        country_letters = update_guess(country_letters, guess)
-        display = def_display_line(country_letters)
+        selected_country["letters"] = update_guess(selected_country["letters"], guess)
+        selected_country["display"] = def_display_line(selected_country["letters"])
 
-        if guess == country_name:
-            display = country_name
-        print(display)
+        if guess == selected_country["name"].upper():
+            selected_country["display"] = selected_country["name"].upper()
 
-        if guess not in display:
+        if guess not in selected_country["display"]:
             lives -= 1
             if len(guess) == 1:
                 print(f"Letter *{guess.upper()}* is not in the word, you lose a life.")
             else:
                 print(f"{guess} is incorrect guess, you lose a life.")
+            if 1 <= lives <= 4:
+                my_hints.append(give_a_hint(hints, selected_country))
 
-            if 1 < lives <= 4:
-                give_a_hint(hints, selected_country)
+        game_over = lose_or_win(lives, game_over)
 
-            if lives == 0:
-                game_over = True
-                print(f"{STARS} IT WAS <{country_name.upper()}>! YOU LOSE {STARS} ")
-        print(stages[lives])
+    if "_" in selected_country["display"]:
+        loses += 1
+        streak = 0
+    else:
+        wins += 1
+        streak += 1
 
-        if "_" not in display:
-            game_over = True
-            print(f"{STARS}  YOU WIN {STARS}")
+    give_country_info(selected_country)
+    print(f"\nWins: {wins}\nLoses: {loses}\nStreak: {streak}")
+    game_on = input("\nPress 'Y' to play again, press any other key to exit: ").lower() == "y"
 
-    print(f"\n{country_name} is located in {selected_country["region"]}, it's capital is {selected_country["capital"]}, "
-                      f"official language is {selected_country["language"]["name"]} and currency is {selected_country["currency"]["name"]}!\n")
-
-    try_again = input("Press 'Y' to play again, press any other key to exit: ").lower()
-    if try_again != "y":
-        game_on = False
-
-print(f"{STARS} Bye-bye! {STARS}")
+print_footer()
