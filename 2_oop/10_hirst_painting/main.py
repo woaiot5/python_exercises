c38e110a31a45dcdb1550e2d2795635314117e9a
woaiot5 <tmtwabp@gmail.com>
2025-04-07 21:29:10 +0200

    better readability and new pictures

--- 2_oop/10_hirst_painting/main.py
+++ 2_oop/10_hirst_painting/main.py
@@ -1,49 +1,82 @@
 import random
-import colorgram
+from colorgram import extract
 import turtle as t
+from os import listdir
 
 
-def boarder(tr,size,w,h):
-    tr.pencolor(colors[random.randint(0,9)].rgb)
-    tr.goto(-w / 2, h/2)
+def random_image_extract(nr_clrs):
+    global img
+    dir_list = listdir("./images")
+    images = []
+    for item in dir_list:
+        if ".jpg" in item:
+            images.append(item)
+    img = random.choice(images)
+    print("Image selected\n...")
+    colors_list = extract(f"./images/{img}", nr_clrs)
+    print("Colors extracted")
+    return colors_list
+
+
+def random_color(colors_list):
+    return colors_list[random.randint(0,len(colors_list) - 1)].rgb
+
+
+def boarder(tr, width, height, clrs):
+    tr.pencolor(random_color(clrs))
+    tr.goto(-width / 2, height / 2)
     tr.pendown()
-    tr.pensize(size)
     for _ in range(2):
-        tr.forward(w)
+        tr.forward(width)
         tr.right(90)
-        tr.forward(h)
+        tr.forward(height)
         tr.right(90)
-    tr.pensize(1)
     tr.penup()
-    tr.goto(0,0)
 
-def draw_a_line(tr, nr_dots, step, rad, colors_list):
+
+def draw_a_line(tr, nr_dots, step, rad, clrs):
     for _ in range(nr_dots):
         tr.fd(step+radius*2)
         tr.pendown()
-        tr.begin_fill()
-        color = colors_list[random.randint(0,9)].rgb
-        dot.pencolor(color)
-        dot.fillcolor(color)
-        tr.circle(rad)
-        tr.end_fill()
+        tr.dot(rad*2, random_color(clrs))
         tr.penup()
 
-def next_line(tr, nr_line, diameter, step_x, step_down):
-    if nr_line%2 == 0:
-        tr.right(90)
-        tr.forward(step_down)
-        tr.right(90)
-        tr.bk(step_x+diameter)
-    else:
-        tr.left(90)
-        tr.forward(step_down+diameter*2)
-        tr.left(90)
-        tr.bk(step_x+diameter)
 
-images = ["1","2","3","4","5"]
-colors = colorgram.extract(f"{random.choice(images)}.jpg", 10)
+def next_line(tr, nr_line, rad, step_back, step_down):
+    right_or_left = {
+        "right": tr.right,
+        "left": tr.left}
+    rl = "right" if nr_line%2 == 0 else "left"
 
+    right_or_left[rl](90)
+    tr.forward(step_down + rad*2)
+    right_or_left[rl](90)
+    tr.bk(step_back + rad*2)
+
+
+def start(tr, width, height, st_x, st_y, rad, clrs):
+    tr.ht()
+    tr.penup()
+    tr.speed("fastest")
+    boarder(tr, width, height, clrs)
+    start_x = -(width / 2) - st_x / 2 - rad
+    start_y = (height / 2) - st_y / 2 - rad
+    tr.goto(start_x, start_y)
+
+
+def draw_dots(tr, nr_l, nr_cir, st_x, st_y, rad, clrs):
+    line = 1
+    while line <= nr_l:
+        draw_a_line(tr, nr_cir, st_x, rad, clrs)
+        line += 1
+        next_line(tr, line, rad, st_x, st_y)
+
+
+img = ''
+
+
+nr_colors = int(input("How many colors? (int): "))
+colors = random_image_extract(nr_colors)
 
 w = int(input("Screen width (int): "))
 h = int(input("Screen height (int): "))
@@ -51,32 +84,18 @@ radius = int(input("Radius of each circle (int): "))
 diameter = radius * 2
 nr_lines = int(input(f"Number of lines (int), max {int(h/diameter)-1} lines: "))
 nr_circles = int(input(f"Number of circles in line (int), max {int(w/diameter)-1} circles: "))
+step_x = (w -  nr_circles*diameter)/nr_circles
+step_y = (h - nr_lines*diameter)/nr_lines
 
 
 t.colormode(255)
+dot = t.Turtle()
 screen = t.Screen()
 screen.setup(width=w, height=h)
-screen.title('Hirst painting')
-
-dot = t.Turtle()
-dot.ht()
-dot.penup()
-dot.speed("fastest")
-# boarder(dot, 1, w, h)
+screen.title(f'Hirst painting - {img}')
 
 
-step_x = (screen.window_width() -  nr_circles*diameter)/nr_circles
-step_y = (screen.window_height() - nr_lines*diameter)/nr_lines
-
-start_x = -(w / 2) - step_x/2 - radius
-start_y = (h / 2) - (step_y/2+diameter)
-
-dot.goto(start_x, start_y)
-
-line = 1
-while line <= nr_lines:
-    draw_a_line(dot, nr_circles, step_x, radius, colors)
-    line +=1
-    next_line(dot, line, diameter, step_x, step_y)
+start(dot, w, h, step_x, step_y, radius, colors)
+draw_dots(dot, nr_lines, nr_circles, step_x, step_y, radius, colors)
 
 screen.exitonclick()
