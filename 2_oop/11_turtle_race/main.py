d8a8c3346e440eb3d7722c98f1ca96e552fbd7bc
woaiot5 <tmtwabp@gmail.com>
2025-04-07 23:30:22 +0200

    last update

--- 2_oop/11_turtle_race/main.py
+++ 2_oop/11_turtle_race/main.py
@@ -3,15 +3,14 @@ from random import randint
 
 
 def prepare_turtles(colors_list, w, h):
-    new_turtles = {}
+    new_turtles = []
     for i in range(0, len(colors_list)):
-        new_turtles[i] = Turtle()
-        new_turtles[i].shape("turtle")
-        new_turtles[i].shapesize(2, 2, 2)
-        new_turtles[i].speed("fast")
-        new_turtles[i].penup()
-        new_turtles[i].color(colors_list[i])
-        new_turtles[i].setpos(-w / 2 + 30, h / 2 - 100 - i * 100)
+        new_turtle = Turtle(shape="turtle")
+        new_turtle.color(colors_list[i])
+        new_turtle.shapesize(2)
+        new_turtle.penup()
+        new_turtle.setpos(-w / 2 + 30, h / 2 - 100 - i * 100)
+        new_turtles.append(new_turtle)
     return new_turtles
 
 
@@ -24,18 +23,18 @@ def draw_line_height(turtle, color, size):
     turtle.penup()
 
 
-def draw_finish_line():
-    finish_line = Turtle()
+def draw_finish_line(start_color, finish_color):
+    finish_line = Turtle(visible=False)
     finish_line.ht()
     finish_line.penup()
     finish_line.speed("fastest")
     x = width / 2 - 50
     y = height / 2
     finish_line.goto(x, y)
-    draw_line_height(finish_line, "black", 5)
+    draw_line_height(finish_line, finish_color, 5)
     x = -width / 2 + 50
     finish_line.goto(x, y)
-    draw_line_height(finish_line, "grey", 2)
+    draw_line_height(finish_line, start_color, 2)
 
 
 def random_forward(turtle):
@@ -51,7 +50,7 @@ def turtles_run(all_turtles):
             random_forward(all_turtles[i])
             if all_turtles[i].xcor() > max_x:
                 max_x = all_turtles[i].xcor()
-                winner = all_turtles[i].color()[0]
+                winner = all_turtles[i].pencolor()
             elif all_turtles[i].xcor() == max_x:
                 winner += f" and {all_turtles[i].color()[0]}"
 
@@ -65,9 +64,10 @@ score = 0
 games_played = 0
 game_on = True
 
+
 while game_on:
     screen.title(f"Turtle Race! Your score: {score}/{games_played}")
-    draw_finish_line()
+    draw_finish_line("grey", "orange")
     turtles = prepare_turtles(colors,width,height)
 
     my_bet = screen.textinput("Bet", f"Bet on a turtle!\n{"/".join(colors)}:")
